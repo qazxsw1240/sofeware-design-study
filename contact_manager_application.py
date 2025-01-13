971bc7a976b744adfcffc0bfed3f19510fc31f79
qazxsw1240 <qazxsw010528@gmail.com>
2025-01-14 07:09:10 +0900

    apply oop to contact_manager_application.py

--- contact_manager_application.py
+++ contact_manager_application.py
@@ -1,5 +1,7 @@
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Self
+from enum import IntEnum
+from typing import Iterable, Iterator, Optional, Self, Sized, override
 
 
 @dataclass
@@ -12,83 +14,204 @@ class Contact:
     phone_address: str
 
 
-class ContactManagerApplication:
+class ContactCollection(Sized, Iterable[Contact]):
     contacts: list[Contact]
 
-    def __init__(self: Self) -> None:
-        self.contacts = []
+    def __init__(self: Self, *contacts: Contact) -> None:
+        self.contacts = list(contacts)
+
+    @property
+    def empty(self: Self) -> bool:
+        return len(self.contacts) == 0
+
+    def find(self: Self, name: str) -> Optional[Contact]:
+        for contact in self.contacts:
+            if contact.name == name:
+                return contact
+        return None
+
+    def add(self: Self, contact: Contact) -> bool:
+        if contact in self.contacts:
+            return False
+        self.contacts.append(contact)
+        return True
+
+    def remove(self: Self, contact: Contact) -> bool:
+        if contact not in self.contacts:
+            return False
+        self.contacts.remove(contact)
+        return True
+
+    @override
+    def __len__(self: Self) -> int:
+        return len(self.contacts)
+
+    @override
+    def __iter__(self) -> Iterator[Contact]:
+        return iter(self.contacts)
+
+
+class ContactStrategyResult(IntEnum):
+    CONTINUE = 0
+    EXIT = 1
+
+
+class ContactStrategy(ABC):
+    @property
+    @abstractmethod
+    def description(self: Self) -> str:
+        pass
+
+    @abstractmethod
+    def handle_contacts(
+            self: Self,
+            contacts: ContactCollection) -> ContactStrategyResult:
+        pass
+
+
+class ContactManagerApplication:
+    contacts: ContactCollection
+    strategies: list[ContactStrategy]
+
+    def __init__(self: Self, *strategies: ContactStrategy) -> None:
+        self.contacts = ContactCollection()
+        self.strategies = list(strategies)
 
     def run(self: Self):
-        command = self.fetch_command()
-        while command != 5:
-            if command == 1:
-                print("Print Contacts:")
-                if len(self.contacts) == 0:
-                    print("\tNo contact is registered.")
-                else:
-                    for contact in self.contacts:
-                        print(f"\t{contact}")
-            elif command == 2:
-                print("Add Contact:")
-                print("\tInput the name for new contact:")
-                name = input(">>")
-                print("\tInput the phone address for new contact:")
-                phone_address = input(">>")
-                contact = Contact(name, phone_address)
-                self.contacts.append(contact)
-            elif command == 3:
-                print("Remove Contact:")
-                print("\tInput the name to remove contact:")
-                name = input(">>")
-                target_contact = None
-                for contact in self.contacts:
-                    if contact.name == name:
-                        target_contact = contact
-                        break
-                if target_contact is None:
-                    print(f'Cannot find any contacts with the name "{name}".')
-                else:
-                    self.contacts.remove(target_contact)
-                    print("\tSuccessfully removed the contact.")
-            elif command == 4:
-                print("Update Contact:")
-                print("\tInput the name to update contact:")
-                name = input(">>")
-                target_contact = None
-                for contact in self.contacts:
-                    if contact.name == name:
-                        target_contact = contact
-                        break
-                if target_contact is None:
-                    print(f'Cannot find any contacts with then name "{name}".')
-                else:
-                    print("\tInput a new address for the contact:")
-                    new_address = input(">>")
-                    target_contact.phone_address = new_address
-                    print("\tSuccessfully updated the contact.")
+        result = ContactStrategyResult.CONTINUE
+        while result != ContactStrategyResult.EXIT:
             command = self.fetch_command()
+            strategy = self.strategies[command - 1]
+            result = strategy.handle_contacts(self.contacts)
 
     def print_task_info(self: Self) -> None:
         print("------------------------------------------------")
         print("Input the number for the task which you want.")
-        print("1. Print contacts")
-        print("2. Add contact")
-        print("3. Remove contact")
-        print("4. Update contact")
-        print("5. Exit")
+        for i, strategy in enumerate(self.strategies):
+            print(f"{i + 1}. {strategy.description}")
 
     def fetch_command(self: Self) -> int:
         self.print_task_info()
         command = int(input(">>"))
-        while command < 1 or command > 5:
+        while command < 1 or command > len(self.strategies):
             print("Invalid input found.")
             self.print_task_info()
             command = int(input(">>"))
         return command
 
 
+class ContactPrintStrategy(ContactStrategy):
+    @property
+    @override
+    def description(self: Self) -> str:
+        return "Print Contacts"
+
+    @override
+    def handle_contacts(
+            self: Self,
+            contacts: ContactCollection) -> ContactStrategyResult:
+        print(f"{self.description}:")
+        if contacts.empty:
+            print("\tNo contact is registered.")
+        else:
+            for contact in contacts:
+                print(f"\t{contact}")
+        return ContactStrategyResult.CONTINUE
+
+
+class ContactAddStrategy(ContactStrategy):
+    @property
+    @override
+    def description(self: Self) -> str:
+        return "Add Contact"
+
+    @override
+    def handle_contacts(
+            self: Self,
+            contacts: ContactCollection) -> ContactStrategyResult:
+        print(f"{self.description}:")
+        print("\tInput the name for new contact:")
+        name = input(">>")
+        print("\tInput the phone address for new contact:")
+        phone_address = input(">>")
+        contact = Contact(name, phone_address)
+        contacts.add(contact)
+        return ContactStrategyResult.CONTINUE
+
+
+class ContactRemoveStrategy(ContactStrategy):
+    @property
+    @override
+    def description(self: Self) -> str:
+        return "Remove Contact"
+
+    @override
+    def handle_contacts(
+            self: Self,
+            contacts: ContactCollection) -> ContactStrategyResult:
+        print(f"{self.description}:")
+        print("\tInput the name to remove contact:")
+        name = input(">>")
+        target_contact = None
+        for contact in contacts:
+            if contact.name == name:
+                target_contact = contact
+                break
+        if target_contact is None:
+            print(f'Cannot find any contacts with the name "{name}".')
+        else:
+            contacts.remove(target_contact)
+            print("\tSuccessfully removed the contact.")
+        return ContactStrategyResult.CONTINUE
+
+
+class ContactUpdateStrategy(ContactStrategy):
+    @property
+    @override
+    def description(self: Self) -> str:
+        return "Update Contact"
+
+    def handle_contacts(
+            self: Self,
+            contacts: ContactCollection) -> ContactStrategyResult:
+        print(f"{self.description}:")
+        print("\tInput the name to update contact:")
+        name = input(">>")
+        target_contact = None
+        for contact in contacts:
+            if contact.name == name:
+                target_contact = contact
+                break
+        if target_contact is None:
+            print(f'Cannot find any contacts with then name "{name}".')
+        else:
+            print("\tInput a new address for the contact:")
+            new_address = input(">>")
+            target_contact.phone_address = new_address
+            print("\tSuccessfully updated the contact.")
+        return ContactStrategyResult.CONTINUE
+
+
+class ContactExitStrategy(ContactStrategy):
+    @property
+    @override
+    def description(self: Self) -> str:
+        return "Exit"
+
+    @override
+    def handle_contacts(
+            self: Self,
+            contacts: ContactCollection) -> ContactStrategyResult:
+        return ContactStrategyResult.EXIT
+
+
 def main():
-    app = ContactManagerApplication()
+    app = ContactManagerApplication(
+        ContactPrintStrategy(),
+        ContactAddStrategy(),
+        ContactRemoveStrategy(),
+        ContactUpdateStrategy(),
+        ContactExitStrategy())
     app.run()
 
 
