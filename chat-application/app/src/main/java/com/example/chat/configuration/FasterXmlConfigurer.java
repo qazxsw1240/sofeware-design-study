ac5c202d99870e23cb96ade6687c8d6fc0a0d7d8
qazxsw1240 <qazxsw010528@gmail.com>
2025-01-14 20:57:39 +0900

    update FasterXmlConfigurer

--- chat-application/app/src/main/java/com/example/chat/configuration/FasterXmlConfigurer.java
+++ chat-application/app/src/main/java/com/example/chat/configuration/FasterXmlConfigurer.java
@@ -1,9 +1,13 @@
 package com.example.chat.configuration;
 
+import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
 
 @Configuration
 public class FasterXmlConfigurer {
@@ -12,7 +16,16 @@ public class FasterXmlConfigurer {
     public ObjectMapper getObjectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.disable(SerializationFeature.INDENT_OUTPUT);
+        objectMapper.registerModule(getSimpleModule());
         return objectMapper;
+    }
+
+    @Bean
+    Module getSimpleModule() {
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer());
+        module.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer());
+        return module;
     }
 
 }
