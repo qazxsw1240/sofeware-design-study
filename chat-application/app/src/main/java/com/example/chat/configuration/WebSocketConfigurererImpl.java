02c39310ba08bd2a49482b90f5e4ecd738814b08
qazxsw1240 <qazxsw010528@gmail.com>
2025-01-14 15:31:55 +0900

    update WebSocketConfigurerImpl

--- chat-application/app/src/main/java/com/example/chat/configuration/WebSocketConfigurererImpl.java
+++ chat-application/app/src/main/java/com/example/chat/configuration/WebSocketConfigurererImpl.java
@@ -7,11 +7,18 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+import com.example.chat.websocket.WebSocketEventListenerManager;
 import com.example.chat.websocket.WebSocketHandlerImpl;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfigurererImpl implements WebSocketConfigurer {
+
+    private final WebSocketHandlerImpl webSocketHandler;
+
+    public WebSocketConfigurererImpl() {
+        this.webSocketHandler = new WebSocketHandlerImpl();
+    }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
@@ -20,7 +27,12 @@ public class WebSocketConfigurererImpl implements WebSocketConfigurer {
 
     @Bean
     WebSocketHandler getWebSocketHandler() {
-        return new WebSocketHandlerImpl();
+        return this.webSocketHandler;
+    }
+
+    @Bean
+    WebSocketEventListenerManager getWebSocketEventListenerManager() {
+        return this.webSocketHandler;
     }
 
 }
