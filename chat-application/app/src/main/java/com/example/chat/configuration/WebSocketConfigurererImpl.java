2be014450bd00b5927897d5c37d5b9e1b3ab6b9f
qazxsw1240 <qazxsw010528@gmail.com>
2025-01-14 20:19:38 +0900

    update WebSocketConfigurererImpl

--- chat-application/app/src/main/java/com/example/chat/configuration/WebSocketConfigurererImpl.java
+++ chat-application/app/src/main/java/com/example/chat/configuration/WebSocketConfigurererImpl.java
@@ -1,18 +1,20 @@
 package com.example.chat.configuration;
 
+import com.example.chat.websocket.WebSocketEventListenerManager;
+import com.example.chat.websocket.WebSocketHandlerImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-
-import com.example.chat.websocket.WebSocketEventListenerManager;
-import com.example.chat.websocket.WebSocketHandlerImpl;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfigurererImpl implements WebSocketConfigurer {
+
+    private static final int MAX_BUFFER_SIZE = 8192;
 
     private final WebSocketHandlerImpl webSocketHandler;
 
@@ -22,7 +24,17 @@ public class WebSocketConfigurererImpl implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(getWebSocketHandler(), "/chat").setAllowedOrigins("*");
+        registry
+                .addHandler(getWebSocketHandler(), "/chat")
+                .setAllowedOrigins("*");
+    }
+
+    @Bean
+    public ServletServerContainerFactoryBean createWebSocketContainer() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(MAX_BUFFER_SIZE);
+        container.setMaxBinaryMessageBufferSize(MAX_BUFFER_SIZE);
+        return container;
     }
 
     @Bean
