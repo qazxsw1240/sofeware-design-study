a66670959075e36bfd699e54712963657632e8d3
qazxsw1240 <qazxsw010528@gmail.com>
2025-01-14 20:57:32 +0900

    add LocalDateTimeSerializer && LocalDateTimeDeserializer

---
+++ chat-application/app/src/main/java/com/example/chat/configuration/LocalDateTimeDeserializer.java
@@ -0,0 +1,19 @@
+package com.example.chat.configuration;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime> {
+
+    @Override
+    public LocalDateTime deserialize(JsonParser p, DeserializationContext context) throws IOException, JacksonException {
+        return LocalDateTime.parse(p.getText(), DateTimeFormatter.ISO_DATE_TIME);
+    }
+
+}
